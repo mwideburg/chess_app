5a8f51d6f842cc960fe301fd2367d1d96c7f88ac
rmiglin <rossmiglin@gmail.com>
2020-12-02 11:54:53 -0500

    working skeleton

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys_dev').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mwideburg:0xnznnrPkVxHuMFd@mta.0w3kl.mongodb.net/MTA_trackerdb?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
+
+
+
